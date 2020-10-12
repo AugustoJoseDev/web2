56a73dafdb04af37e2bf82782047322ff6d6f18a
José Augusto <jose.augusto.comparotto@gmail.com>
2020-10-12 00:08:04 -0400

    bootstpat navbar

--- server.js
+++ server.js
@@ -2,14 +2,15 @@ const express = require("express")
 
 const app = express()
 
-app.get('/', (req, res) => {
+app.use('/', express.static(__dirname + "/public"))
 
-    return res.sendFile(__dirname + "/public/index.html")
-
-})
+app.use('/js', express.static(__dirname + "/node_modules/bootstrap/dist/js") )
+app.use('/css', express.static(__dirname + "/node_modules/bootstrap/dist/css") )
 
 app.listen(5000, () => {
 
     console.log("Escutando em http://localhost:5000")
 
 })
+
+// https://getbootstrap.com/docs/4.5/components/navbar
