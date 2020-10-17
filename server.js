d9a048e0e49258e77437f51a036e018cf754c6fd
José Augusto Gomes <jose.gomes3@estudante.ifms.edu.br>
2020-10-17 11:25:23 -0400

    rotas

--- server.js
+++ server.js
@@ -3,11 +3,14 @@ const express = require("express")
 const app = express()
 
 app.use('/', express.static(__dirname + "/public"))
+app.use('/favicon.png', express.static(__dirname + "/favicon.png"))
 
-app.use('/bootstrap/css', express.static(__dirname + "/node_modules/bootstrap/dist/css/bootstrap.min.css"))
-app.use('/bootstrap/js', express.static(__dirname + "/node_modules/bootstrap/dist/js/bootstrap.min.js"))
-app.use('/jquery', express.static(__dirname + "/node_modules/jquery/dist/jquery.slim.min.js"))
-app.use('/popper', express.static(__dirname + "/node_modules/popper.js/dist/popper.min.js"))
+app.use('/bootstrap.css', express.static(__dirname + "/node_modules/bootstrap/dist/css/bootstrap.min.css"))
+
+app.use('/bootstrap.js', express.static(__dirname + "/node_modules/bootstrap/dist/js/bootstrap.min.js"))
+app.use('/jquery.js', express.static(__dirname + "/node_modules/jquery/dist/jquery.slim.min.js"))
+app.use('/popper.js', express.static(__dirname + "/node_modules/popper.js/dist/popper.min.js"))
+
 
 app.listen(5000, () => {
 
