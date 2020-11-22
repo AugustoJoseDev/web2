667baa7c6d1d45b822a5ef8dbb906aa575afe00f
José Augusto <jose.augusto.comparotto@gmail.com>
2020-11-22 13:48:15 -0400

    url amigavel

--- src/data.js
+++ src/data.js
@@ -1,11 +1,15 @@
 const pages = require('./pages.json')
 
 function getPageData(nomeEquipe){
-    let filtered = pages.filter(e => e.equipe.toLocaleLowerCase() == nomeEquipe.toLocaleLowerCase())
+    let filtered = pages.filter(e => beautify(e.equipe) == beautify(nomeEquipe) )
     
     if(filtered.length !== 1) return {error:404}
 
     return filtered[0]
+}
+
+function beautify(str){
+    return str.toLowerCase().replace(/\s+/g,'-').normalize("NFD").replace(/[^a-zA-Z-]/g,"")
 }
 
 module.exports = {getPageData}
